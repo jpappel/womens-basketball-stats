c617ea1aee2b8cc10485cc380a97ffa6500649e4
JP Appel <jeanpierre.appel01@gmail.com>
2024-02-22 10:35:03 -0500

    created RosterDataManager Interface and updated docs

--- app/src/main/java/Database.java
+++ app/src/main/java/Database.java
@@ -6,7 +6,11 @@ import java.sql.Statement;
 public class Database {
     private static final String DB_URL = "jdbc:sqlite:WomenBBall.db";
 
-    // Establish connection to the SQLite database
+    /**
+        * Establishes a connection to the database.
+        *
+        * @return the Connection object representing the database connection, or null if an error occurs.
+        */
     public static Connection connect() {
         try {
             return DriverManager.getConnection(DB_URL);
@@ -16,7 +20,18 @@ public class Database {
         }
     }
 
-    // Creating Players Table
+    /**
+     * Creates a table named "Players" in the database if it does not already exist.
+     * The table has the following columns:
+     * - id: INTEGER (Primary Key)
+     * - firstName: VARCHAR(20) (Not Null)
+     * - lastName: VARCHAR(20) (Not Null)
+     * - position: VARCHAR(30) (Not Null)
+     * - seniority: CHAR(8) (Not Null)
+     * - playerNum: INT (Not Null)
+     *
+     * @param conn the Connection object representing the database connection
+     */
     public static void createTable(Connection conn) {
         String sql = "CREATE TABLE IF NOT EXISTS Players (\n"
                 + " id INTEGER PRIMARY KEY,\n"
