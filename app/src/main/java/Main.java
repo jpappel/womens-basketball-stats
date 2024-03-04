09c11f7c7fe5be671e7376a6f19e85b8cfc90863
Alan <fuentesa02@moravian.edu>
2024-03-04 13:26:19 -0500

    Modified Database file to DatabaseManager which handles connection and creating table, Created DBTableManager which interacts with RosterDataManager and handles methods to add/remove/update player/roster info, refactored try-catch block for sql statement execution within Databasemanger file; error currently with return roster statement in getRoster method (DBTableManager)

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -13,12 +13,12 @@ public class Main {
     public static void main(String[] args) {
 
         // Connect to the SQLite database
-        try (Connection conn = Database.connect()) {
+        try (Connection conn = DatabaseManager.connect()) {
             if (conn != null) {
                 System.out.println("Connected to the database.");
 
                 // Creates Players table if it doesn't exist
-                Database.createTable(conn);
+                DatabaseManager.createTable(conn);
 
             } else {
                 System.out.println("Failed to connect to the database.");
