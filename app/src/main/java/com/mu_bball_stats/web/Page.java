0f85c009373f9ed4ae82b7b68c7d20e09ec86e5c
JP Appel <jeanpierre.appel01@gmail.com>
2024-03-27 22:48:23 -0400

    created new page class

---
+++ app/src/main/java/com/mu_bball_stats/web/Page.java
@@ -0,0 +1,27 @@
+package com.mu_bball_stats.web;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Page {
+    protected String title;
+    protected List<String> scripts;
+
+    public Page(String title) {
+        this.title = title;
+        this.scripts = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void addScript(String script) {
+        scripts.add(script);
+    }
+
+    public List<String> getScripts() {
+        return scripts;
+    }
+
+}
